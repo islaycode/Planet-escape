ec370f50cb78228b2d159f7a75460aa1b62e263f
Nikhil <nikzdev@gmail.com>
2020-07-05 18:13:50 +0530

    Adding rotation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,6 +9,11 @@ public class Player : MonoBehaviour {
     [Tooltip("In m^-1")][SerializeField] float speed = 10f; // Multiply this for increasing speed of senstivity 
     [Tooltip("In m")] [SerializeField] float xRange = 3f;
     [Tooltip("In m")] [SerializeField] float yRange = 3f;
+    [SerializeField] float positionPitch = -15;
+    [SerializeField] float controlPitch = -30f;
+    [SerializeField] float positionYaw = -10f;
+    [SerializeField] float YawPitch = -5f;
+    float xThrow, yThrow;
 
     // Use this for initialization
     void Start () {
@@ -24,18 +29,21 @@ public class Player : MonoBehaviour {
 
     private void ProcessRotation()
     {
-        transform.localRotation = Quaternion.Euler(-30f,30f,0f); 
+        float pitch = transform.localPosition.y * positionPitch + yThrow *controlPitch;
+        float yaw = transform.localPosition.x *positionYaw + xThrow *YawPitch ;
+        float roll = 0;
+        transform.localRotation = Quaternion.Euler(pitch,yaw,roll); 
     }
 
     private void ProcessTranslation()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * speed * Time.deltaTime;
 
         float rawPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawPos, -xRange, xRange);
 
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = yThrow * speed * Time.deltaTime;
 
         float rawPos2 = transform.localPosition.y + yOffset;
