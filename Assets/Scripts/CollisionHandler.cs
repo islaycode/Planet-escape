d6c56940de56b112f43740576f49c71e16c9c573
Nikhil <nikzdev@gmail.com>
2020-07-08 01:06:40 +0530

    Adde Reload scene

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour {
+
+    [Tooltip("In seconds")] [SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")] [SerializeField] GameObject deathFx;
 
     void OnTriggerEnter(Collider other)
     {
         DeathSquence();
+        deathFx.SetActive(true);
+        Invoke("ReloadLevel", levelLoadDelay);
     }
     private void DeathSquence()
     {
         print("Player Died");
         SendMessage("OnPlayerDeath");
+    }
+    private void ReloadLevel() // string reference
+    {
+        SceneManager.LoadScene(1);
     }
 }
