696c5dc92724f6179bc5a1c76379217d33748046
Nikhil <nikzdev@gmail.com>
2020-07-08 01:37:59 +0530

    Added singleton on music.cs now if there is more than 1 music player it gets destroyed

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -4,6 +4,15 @@ public class Music : MonoBehaviour {
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        int numMusicPlayer = FindObjectsOfType<Music>().Length; // counting  number of music players in our game
+        if (numMusicPlayer >1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+       
     }
 }
